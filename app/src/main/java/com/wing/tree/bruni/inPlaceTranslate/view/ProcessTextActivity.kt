7e6206073693a2acadaecbe3776494937a86ecc2
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-12-17 19:59:02 +0900

    refactor : Adjust AdView size and modify the code to load AdView

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/ProcessTextActivity.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/ProcessTextActivity.kt
@@ -4,22 +4,35 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.method.MovementMethod
+import android.text.method.ScrollingMovementMethod
+import android.transition.TransitionManager
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.wing.tree.bruni.core.constant.ZERO
+import com.wing.tree.bruni.core.extension.context
 import com.wing.tree.bruni.core.extension.string
 import com.wing.tree.bruni.core.extension.then
 import com.wing.tree.bruni.core.useCase.Result
+import com.wing.tree.bruni.inPlaceTranslate.BuildConfig
+import com.wing.tree.bruni.inPlaceTranslate.R
 import com.wing.tree.bruni.inPlaceTranslate.databinding.ActivityProcessTextBinding
 import com.wing.tree.bruni.inPlaceTranslate.viewModel.ProcessTextViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import java.util.Locale
 
 @AndroidEntryPoint
 class ProcessTextActivity : AppCompatActivity() {
@@ -77,7 +90,11 @@ class ProcessTextActivity : AppCompatActivity() {
     }
 
     private fun ActivityProcessTextBinding.bind() {
+        source.text = Locale.getDefault().getDisplayLanguage(Locale.ENGLISH)
+        target.text = Locale.getDefault().getDisplayLanguage(Locale.KOREAN)
+
         bottomSheet()
+        adView()
     }
 
     private fun ActivityProcessTextBinding.bottomSheet() {
@@ -102,6 +119,40 @@ class ProcessTextActivity : AppCompatActivity() {
             },
             DELAY_MILLIS
         )
+
+        translatedText()
+    }
+
+    private fun ActivityProcessTextBinding.translatedText() {
+        translatedText.movementMethod = ScrollingMovementMethod()
+    }
+
+    private fun ActivityProcessTextBinding.adView() {
+        AdView(context).apply {
+            val adRequest = AdRequest.Builder().build()
+
+            @StringRes
+            val resId = if (BuildConfig.DEBUG) {
+                R.string.sample_banner_ad_unit_id
+            } else {
+                R.string.banner_ad_unit_id
+            }
+
+            adUnitId = getString(resId)
+            adListener = object : AdListener() {
+                override fun onAdLoaded() {
+                    super.onAdLoaded()
+
+                    with(frameLayout) {
+                        addView(this@apply)
+                    }
+                }
+            }
+
+            setAdSize(AdSize(AD_WIDTH, AD_HEIGHT))
+
+            loadAd(adRequest)
+        }
     }
 
     private fun ProcessTextViewModel.observe() {
@@ -129,6 +180,8 @@ class ProcessTextActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val AD_HEIGHT = 32
+        private const val AD_WIDTH = 320
         private const val DELAY_MILLIS = 150L
     }
 }
