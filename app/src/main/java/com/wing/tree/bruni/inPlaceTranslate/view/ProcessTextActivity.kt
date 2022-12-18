6d1f8c892c23f2f388892da59cd503eb27eb3c02
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-12-18 14:12:58 +0900

    feat : Add tts feature

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/ProcessTextActivity.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/ProcessTextActivity.kt
@@ -4,15 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.text.method.MovementMethod
+import android.speech.tts.TextToSpeech
 import android.text.method.ScrollingMovementMethod
-import android.transition.TransitionManager
 import android.view.View
+import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
@@ -21,18 +20,21 @@ import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.AdView
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.wing.tree.bruni.core.constant.NEWLINE
 import com.wing.tree.bruni.core.constant.ZERO
 import com.wing.tree.bruni.core.extension.context
+import com.wing.tree.bruni.core.extension.copyToClipboard
 import com.wing.tree.bruni.core.extension.string
 import com.wing.tree.bruni.core.extension.then
 import com.wing.tree.bruni.core.useCase.Result
 import com.wing.tree.bruni.inPlaceTranslate.BuildConfig
 import com.wing.tree.bruni.inPlaceTranslate.R
 import com.wing.tree.bruni.inPlaceTranslate.databinding.ActivityProcessTextBinding
+import com.wing.tree.bruni.inPlaceTranslate.extension.letIsViewGroup
 import com.wing.tree.bruni.inPlaceTranslate.viewModel.ProcessTextViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
-import java.util.Locale
+import java.util.*
 
 @AndroidEntryPoint
 class ProcessTextActivity : AppCompatActivity() {
@@ -57,22 +59,35 @@ class ProcessTextActivity : AppCompatActivity() {
 
     private val viewModel by viewModels<ProcessTextViewModel>()
 
+    private var textToSpeech: TextToSpeech? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+
         viewBinding.bind()
+        viewModel.observe()
 
-        with(viewModel) {
-            processText?.let {
-                translate(it.string)
-            } ?: finishThen {
-                overridePendingTransition(ZERO, ZERO)
-                return
-            }
-
-            observe()
+        processText?.let {
+            viewModel.translate(it.string)
+        } ?: finishThen {
+            overridePendingTransition(ZERO, ZERO)
+            return
         }
+
+        textToSpeech = TextToSpeech(this) { status ->
+
+        }
+    }
+
+    override fun onDestroy() {
+        textToSpeech?.let {
+            it.stop()
+            it.shutdown()
+        }
+
+        super.onDestroy()
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -85,6 +100,20 @@ class ProcessTextActivity : AppCompatActivity() {
         } ?: finishThen { overridePendingTransition(ZERO, ZERO) }
     }
 
+    private fun speak(loc: Locale, text: CharSequence) = textToSpeech?.let {
+        when(it.setLanguage(loc)) {
+            TextToSpeech.LANG_MISSING_DATA, TextToSpeech.LANG_NOT_SUPPORTED -> {
+
+            }
+            else -> {
+                it.setSpeechRate(SPEECH_RATE)
+                it.setPitch(PITCH)
+
+                it.speak(text, TextToSpeech.QUEUE_FLUSH, null, "id1")
+            }
+        }
+    }
+
     private inline fun finishThen(block: () -> Unit) {
         finish().then { block() }
     }
@@ -94,6 +123,8 @@ class ProcessTextActivity : AppCompatActivity() {
         target.text = Locale.getDefault().getDisplayLanguage(Locale.KOREAN)
 
         bottomSheet()
+        textView()
+        imageButton()
         adView()
     }
 
@@ -119,40 +150,59 @@ class ProcessTextActivity : AppCompatActivity() {
             },
             DELAY_MILLIS
         )
-
-        translatedText()
     }
 
-    private fun ActivityProcessTextBinding.translatedText() {
+    private fun ActivityProcessTextBinding.textView() {
         translatedText.movementMethod = ScrollingMovementMethod()
     }
 
+    private fun ActivityProcessTextBinding.imageButton() {
+        speakSourceText.setOnClickListener {
+            speak(Locale.ENGLISH, sourceText.text)
+        }
+
+        copyToClipboard.setOnClickListener {
+            copyToClipboard(LABEL_PLAIN_TEXT, translatedText.text)
+                .then {
+                    Toast.makeText(
+                        context,
+                        R.string.copied_to_clipboard,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+        }
+
+        speakTranslatedText.setOnClickListener {
+            speak(Locale.KOREA, translatedText.text)
+        }
+    }
+
     private fun ActivityProcessTextBinding.adView() {
+        val adRequest = AdRequest.Builder().build()
+
+        @StringRes
+        val resId = if (BuildConfig.DEBUG) {
+            R.string.sample_banner_ad_unit_id
+        } else {
+            R.string.banner_ad_unit_id
+        }
+
         AdView(context).apply {
-            val adRequest = AdRequest.Builder().build()
-
-            @StringRes
-            val resId = if (BuildConfig.DEBUG) {
-                R.string.sample_banner_ad_unit_id
-            } else {
-                R.string.banner_ad_unit_id
-            }
-
             adUnitId = getString(resId)
             adListener = object : AdListener() {
                 override fun onAdLoaded() {
                     super.onAdLoaded()
 
-                    with(frameLayout) {
-                        addView(this@apply)
+                    parent?.letIsViewGroup {
+                        it.removeView(this@apply)
                     }
+
+                    frameLayout.addView(this@apply)
                 }
             }
 
             setAdSize(AdSize(AD_WIDTH, AD_HEIGHT))
-
-            loadAd(adRequest)
-        }
+        }.loadAd(adRequest)
     }
 
     private fun ProcessTextViewModel.observe() {
@@ -167,8 +217,8 @@ class ProcessTextActivity : AppCompatActivity() {
                             val data = it.data.ifEmpty { return@collect }
                             val translation = data.first()
 
-                            sourceText.setText(translation.sourceText)
-                            translatedText.text = translation.translatedText
+                            sourceText.setText(translation.sourceText.plus(NEWLINE))
+                            translatedText.text = translation.translatedText.plus(NEWLINE)
                         }
                         is Result.Failure -> {
                             viewBinding.circularProgressIndicator.hide()
@@ -183,5 +233,8 @@ class ProcessTextActivity : AppCompatActivity() {
         private const val AD_HEIGHT = 32
         private const val AD_WIDTH = 320
         private const val DELAY_MILLIS = 150L
+        private const val LABEL_PLAIN_TEXT = "text/plain"
+        private const val PITCH = 1.0F
+        private const val SPEECH_RATE = 1.0F
     }
 }
