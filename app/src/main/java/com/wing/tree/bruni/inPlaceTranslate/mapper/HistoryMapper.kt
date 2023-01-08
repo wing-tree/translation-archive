8e09016fc8122e4b01186d50ec755a62c2ad2084
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-08 09:01:52 +0900

    feat : Define HistoryMapper

---
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/mapper/HistoryMapper.kt
@@ -0,0 +1,19 @@
+package com.wing.tree.bruni.inPlaceTranslate.mapper
+
+import com.wing.tree.bruni.inPlaceTranslate.data.mapper.EntityMapper
+import com.wing.tree.bruni.inPlaceTranslate.data.entity.History as Entity
+import com.wing.tree.bruni.inPlaceTranslate.model.History as Model
+
+class HistoryMapper : EntityMapper<Entity, Model> {
+    override fun toModel(entity: Entity): Model {
+        return Model(
+            detectedSourceLanguage = entity.detectedSourceLanguage,
+            isFavorite = entity.isFavorite,
+            source = entity.source,
+            sourceText = entity.sourceText,
+            target = entity.target,
+            translatedAt = entity.translatedAt,
+            translatedText = entity.translatedText
+        )
+    }
+}
