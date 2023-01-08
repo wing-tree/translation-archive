63ef856dd859630b4d65276b1e9f9918e4799d98
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-08 09:05:48 +0900

    feat : Implement HistoryViewModel

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/viewModel/HistoryViewModel.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/viewModel/HistoryViewModel.kt
@@ -1,6 +1,27 @@
 package com.wing.tree.bruni.inPlaceTranslate.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.wing.tree.bruni.inPlaceTranslate.mapper.HistoryMapper
+import com.wing.tree.bruni.inPlaceTranslate.data.entity.History as Entity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
 
-class HistoryViewModel : ViewModel() {
+@HiltViewModel
+class HistoryViewModel @Inject constructor(
+    historyPager: Pager<Int, Entity>
+) : ViewModel() {
+    private val historyMapper = HistoryMapper()
+
+    val historyPagingData = historyPager.flow
+        .map { pagingData ->
+            pagingData.map { entity ->
+                historyMapper.toModel(entity)
+            }
+        }
+        .cachedIn(viewModelScope)
 }
