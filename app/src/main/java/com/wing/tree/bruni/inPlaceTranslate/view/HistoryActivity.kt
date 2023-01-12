b5137cdfff54fdb4f1f727579736865aa4e8495f
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-12 23:09:31 +0900

    feat : Implement HistoryActivity adapter

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/HistoryActivity.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/HistoryActivity.kt
@@ -1,24 +1,46 @@
 package com.wing.tree.bruni.inPlaceTranslate.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.wing.tree.bruni.inPlaceTranslate.adapter.HistoryPagingDataAdapter
 import com.wing.tree.bruni.inPlaceTranslate.databinding.ActivityHistoryBinding
+import com.wing.tree.bruni.inPlaceTranslate.viewModel.HistoryViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HistoryActivity : AppCompatActivity() {
     private val binding by lazy { ActivityHistoryBinding.inflate(layoutInflater) }
+    private val historyPagingDataAdapter = HistoryPagingDataAdapter()
+    private val viewModel by viewModels<HistoryViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         binding.bind()
+        viewModel.collect()
     }
 
     private fun ActivityHistoryBinding.bind() {
         recyclerView.apply {
+            adapter = historyPagingDataAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
 
+    private fun HistoryViewModel.collect() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                historyPagingData.collect {
+                    historyPagingDataAdapter.submitData(it)
+                }
+            }
         }
     }
 }
