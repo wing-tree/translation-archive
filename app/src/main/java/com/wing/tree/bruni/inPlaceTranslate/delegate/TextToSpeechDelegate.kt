f2c01d63378161177ee0c2c54b379d77d3f2ee5e
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-14 11:14:07 +0900

    refactor : Rename method

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/delegate/TextToSpeechDelegate.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/delegate/TextToSpeechDelegate.kt
@@ -5,6 +5,6 @@ import androidx.appcompat.app.AppCompatActivity
 import java.util.*
 
 interface TextToSpeechDelegate {
-    fun initializeTextToSpeech(appCompatActivity: AppCompatActivity, listener: TextToSpeech.OnInitListener)
+    fun initTextToSpeech(appCompatActivity: AppCompatActivity, listener: TextToSpeech.OnInitListener)
     fun speak(loc: Locale, text: CharSequence)
 }
