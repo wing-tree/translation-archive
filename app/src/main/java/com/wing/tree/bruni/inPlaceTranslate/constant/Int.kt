2b9b08a6ed99ca0958c3e3928c39a92b656489b5
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-14 23:54:34 +0900

    refactor : Change LIMIT_CHARACTERS value

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/constant/Int.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/constant/Int.kt
@@ -1,5 +1,3 @@
 package com.wing.tree.bruni.inPlaceTranslate.constant
 
-import com.wing.tree.bruni.core.constant.ONE_THOUSAND
-
-internal const val LIMIT_CHARACTERS = ONE_THOUSAND
+internal const val LIMIT_CHARACTERS = 2000
