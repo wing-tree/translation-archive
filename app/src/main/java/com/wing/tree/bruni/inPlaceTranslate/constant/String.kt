5d5382634687b4627a6e13a0f9efb63ee1721f43
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-14 23:51:31 +0900

    feat : Create extra key

---
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/constant/String.kt
@@ -0,0 +1,5 @@
+package com.wing.tree.bruni.inPlaceTranslate.constant
+
+private const val PACKAGE_NAME = "com.wing.tree.bruni.inPlaceTranslate.constant"
+
+internal const val EXTRA_HISTORY = "$PACKAGE_NAME.HISTORY"
