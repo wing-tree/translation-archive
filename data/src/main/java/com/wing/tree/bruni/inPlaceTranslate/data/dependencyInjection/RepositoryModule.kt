bcfd9048d22e249ed01ef2670dc12d647b319ce6
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-15 12:34:21 +0900

    feat : Define HistoryRepository injection

--- data/src/main/java/com/wing/tree/bruni/inPlaceTranslate/data/dependencyInjection/RepositoryModule.kt
+++ data/src/main/java/com/wing/tree/bruni/inPlaceTranslate/data/dependencyInjection/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.wing.tree.bruni.inPlaceTranslate.data.dependencyInjection
 
+import com.wing.tree.bruni.inPlaceTranslate.data.repository.HistoryRepositoryImpl
 import com.wing.tree.bruni.inPlaceTranslate.data.repository.PreferencesRepositoryImpl
 import com.wing.tree.bruni.inPlaceTranslate.data.repository.TranslationRepositoryImpl
+import com.wing.tree.bruni.inPlaceTranslate.domain.repository.HistoryRepository
 import com.wing.tree.bruni.inPlaceTranslate.domain.repository.PreferencesRepository
 import com.wing.tree.bruni.inPlaceTranslate.domain.repository.TranslationRepository
 import dagger.Binds
@@ -13,6 +15,10 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 internal abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindsHistoryRepository(historyRepository: HistoryRepositoryImpl): HistoryRepository
+
     @Binds
     @Singleton
     abstract fun bindsPreferencesRepository(preferencesRepository: PreferencesRepositoryImpl): PreferencesRepository
