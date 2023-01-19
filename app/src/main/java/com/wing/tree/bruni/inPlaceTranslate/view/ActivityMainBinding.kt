6f2a9d62e9cfa2e3575a649206b6f2b4191c3bb2
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-20 00:18:05 +0900

    feat : Implement main activity binding

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/ActivityMainBinding.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/ActivityMainBinding.kt
@@ -1,15 +1,21 @@
 package com.wing.tree.bruni.inPlaceTranslate.view
 
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.wing.tree.bruni.core.constant.ONE
 import com.wing.tree.bruni.core.constant.ZERO
 import com.wing.tree.bruni.core.extension.*
+import com.wing.tree.bruni.core.regular.then
 import com.wing.tree.bruni.inPlaceTranslate.R
 import com.wing.tree.bruni.inPlaceTranslate.databinding.ActivityMainBinding
-import com.wing.tree.bruni.inPlaceTranslate.extension.getFloat
+import java.util.concurrent.atomic.AtomicBoolean
 
-internal fun ActivityMainBinding.drawerLayout() = with(requireNotNull(activity)) {
+internal fun ActivityMainBinding.drawerLayout(mainActivity: MainActivity) = with(mainActivity) {
     val actionBarDrawerToggle = ActionBarDrawerToggle(
         this,
         drawerLayout,
@@ -23,20 +29,23 @@ internal fun ActivityMainBinding.drawerLayout() = with(requireNotNull(activity))
     actionBarDrawerToggle.syncState()
 }
 
-internal fun ActivityMainBinding.materialButton() = with(requireNotNull(activity)) {
+internal fun ActivityMainBinding.materialButton(mainActivity: MainActivity) = with(mainActivity) {
+    val swapLanguages = sourceText.swapLanguages
+    val displaySourceLanguage = sourceText.displaySourceLanguage
+    val displayTargetLanguage = translatedText.displayTargetLanguage
+
     swapLanguages.setOnClickListener {
         swapLanguages.isClickable = false
         swapLanguages.isFocusable = false
 
-        val accelerateQuadInterpolator = context.accelerateQuadInterpolator
-        val configShortAnimTime = resources.configShortAnimTime.long
-        val decelerateQuadInterpolator = context.decelerateQuadInterpolator
-        val duration = configShortAnimTime.half
-        val translationY = dimen(R.dimen.text_size_24dp)
+        val accelerateQuadInterpolator = accelerateQuadInterpolator
+        val decelerateQuadInterpolator = decelerateQuadInterpolator
+        val duration = configShortAnimTime.long.half
+        val translationY = dimen(R.dimen.text_size_22dp)
 
         with(displaySourceLanguage) {
             translateDown(duration, translationY, accelerateQuadInterpolator) {
-                swapLanguages()
+                mainActivity.swapLanguages()
                 translateUp(duration, ZERO.float, decelerateQuadInterpolator) {
                     swapLanguages.isClickable = true
                     swapLanguages.isFocusable = true
@@ -53,7 +62,7 @@ internal fun ActivityMainBinding.materialButton() = with(requireNotNull(activity
     }
 }
 
-internal fun ActivityMainBinding.materialToolbar() = with(requireNotNull(activity)) {
+internal fun ActivityMainBinding.materialToolbar(mainActivity: MainActivity) = with(mainActivity) {
     setSupportActionBar(
         materialToolbar.apply {
             setNavigationOnClickListener {
@@ -69,11 +78,53 @@ internal fun ActivityMainBinding.materialToolbar() = with(requireNotNull(activit
     supportActionBar?.setDisplayShowTitleEnabled(false)
 }
 
-internal fun ActivityMainBinding.navigationView() = with(requireNotNull(activity)) {
+internal fun ActivityMainBinding.navigationView(
+    activityResultLauncher: ActivityResultLauncher<Intent>,
+    mainActivity: MainActivity
+) = with(mainActivity) {
+    val shouldCloseDrawer = AtomicBoolean(false)
+
+    lifecycle.addObserver(
+        object : DefaultLifecycleObserver {
+            override fun onStop(owner: LifecycleOwner) {
+                if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    if (shouldCloseDrawer.compareAndSet(true, false)) {
+                        drawerLayout.closeDrawer(GravityCompat.START, false)
+                    }
+                }
+
+                super.onStop(owner)
+            }
+        }
+    )
+
     navigationView.setNavigationItemSelectedListener {
-        when(it.itemId) {
+        when (it.itemId) {
             R.id.history -> {
-                startActivity<HistoryActivity>()
+                activityResultLauncher
+                    .launch(Intent(this, HistoryActivity::class.java))
+                    .then {
+                        overridePendingTransition(
+                            android.R.anim.slide_in_left,
+                            android.R.anim.slide_out_right
+                        )
+
+                        shouldCloseDrawer.set(true)
+                    }
+                false
+            }
+            R.id.clear_all -> {
+                MaterialAlertDialogBuilder(this)
+                    .setTitle(R.string.clear_all)
+                    .setMessage(R.string.clear_all_histories)
+                    .setPositiveButton(R.string.clear_all) { dialog, _ ->
+                        viewModel.clearAllHistories()
+                        dialog.dismiss()
+                    }
+                    .setNegativeButton(R.string.cancel) { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
                 false
             }
             else -> true
@@ -81,23 +132,7 @@ internal fun ActivityMainBinding.navigationView() = with(requireNotNull(activity
     }
 }
 
-internal fun ActivityMainBinding.nestedScrollView() = with(requireNotNull(activity)) {
-    val layoutHeight = dimen(R.dimen.layout_height_60dp)
-    val maximumValue = ONE.float
-    val minimumValue = getFloat(R.dimen.alpha_0_38)
-    val constantOfProportionality = maximumValue
-        .minus(minimumValue)
-        .div(layoutHeight)
-
-    nestedScrollView.setOnScrollChangeListener { _, _, scrollY, _, _ ->
-        val alpha = maximumValue.minus(scrollY.times(constantOfProportionality))
-
-        linearLayout.alpha = alpha.coerceAtLeast(minimumValue)
-    }
-
-    nestedScrollView2.setOnScrollChangeListener { _, _, scrollY, _, _ ->
-        val alpha = maximumValue.minus(scrollY.times(constantOfProportionality))
-
-        linearLayout2.alpha = alpha.coerceAtLeast(minimumValue)
-    }
+internal fun ActivityMainBinding.nestedScrollView(mainActivity: MainActivity) {
+    sourceText.nestedScrollView(mainActivity)
+    translatedText.nestedScrollView(mainActivity)
 }
