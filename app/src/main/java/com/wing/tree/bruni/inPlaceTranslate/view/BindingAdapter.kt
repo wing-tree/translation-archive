4d75746046b8a0204becaadd2c49fb2af12f8b43
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-20 08:02:18 +0900

    feat : Add new binding adapter function

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/BindingAdapter.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/BindingAdapter.kt
@@ -1,9 +1,19 @@
 package com.wing.tree.bruni.inPlaceTranslate.view
 
 import androidx.databinding.BindingAdapter
+import com.google.android.material.progressindicator.CircularProgressIndicator
+import com.wing.tree.bruni.core.useCase.Result
 import com.wing.tree.bruni.inPlaceTranslate.widget.SpeechRecognitionButton
 
 @BindingAdapter("isListening")
 fun setListening(speechRecognitionButton: SpeechRecognitionButton, isListening: Boolean) {
     speechRecognitionButton.isListening = isListening
+}
+
+@BindingAdapter("result")
+fun setResult(circularProgressIndicator: CircularProgressIndicator, result: Result<*>) {
+    when(result) {
+        Result.Loading -> circularProgressIndicator.show()
+        else -> circularProgressIndicator.hide()
+    }
 }
