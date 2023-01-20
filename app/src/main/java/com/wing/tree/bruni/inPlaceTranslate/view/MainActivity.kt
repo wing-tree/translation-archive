cf1e15ee629c17f5f37d305948c4663322ee737f
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-20 17:04:26 +0900

    refactor : Large scale change

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/MainActivity.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/MainActivity.kt
@@ -1,38 +1,60 @@
 package com.wing.tree.bruni.inPlaceTranslate.view
 
 import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import com.google.android.gms.ads.AdSize
 import com.google.android.gms.ads.FullScreenContentCallback
 import com.wing.tree.bruni.core.extension.*
+import com.wing.tree.bruni.core.regular.gone
+import com.wing.tree.bruni.core.regular.visible
 import com.wing.tree.bruni.core.useCase.Result
+import com.wing.tree.bruni.inPlaceTranslate.R
 import com.wing.tree.bruni.inPlaceTranslate.ad.InterstitialAdLoader
 import com.wing.tree.bruni.inPlaceTranslate.ad.InterstitialAdLoaderImpl
-import com.wing.tree.bruni.inPlaceTranslate.constant.LIMIT_CHARACTERS
+import com.wing.tree.bruni.inPlaceTranslate.adapter.HistoryPagingDataAdapter
+import com.wing.tree.bruni.inPlaceTranslate.constant.EXTRA_HISTORY
 import com.wing.tree.bruni.inPlaceTranslate.databinding.ActivityMainBinding
 import com.wing.tree.bruni.inPlaceTranslate.extension.bannerAd
 import com.wing.tree.bruni.inPlaceTranslate.viewModel.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.flow.zip
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : TranslatorActivity(), InterstitialAdLoader by InterstitialAdLoaderImpl() {
     override val viewModel by viewModels<MainViewModel>()
 
+    private val activityResultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result?.resultCode == RESULT_OK) {
+            val data = result.data
+
+            data?.let {
+                val history: HistoryPagingDataAdapter.Item.History =
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                        val clazz = HistoryPagingDataAdapter.Item.History::class.java
+
+                        it.getParcelableExtra(EXTRA_HISTORY, clazz) ?: return@let
+                    } else {
+                        @Suppress("DEPRECATION")
+                        it.getParcelableExtra(EXTRA_HISTORY) ?: return@let
+                    }
+
+                viewModel.translateHistory(history)
+            }
+        }
+    }
+
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
             .also {
-                it.lifecycleOwner = this
                 it.activity = this
+                it.lifecycleOwner = this
                 it.viewModel = viewModel
             }
     }
@@ -48,10 +70,10 @@ class MainActivity : TranslatorActivity(), InterstitialAdLoader by InterstitialA
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.bind()
+        binding.bind(this)
         viewModel.collect()
 
-        processText(intent)
+        translateProcessText(intent)
         initTextToSpeech()
         loadInterstitialAd(this)
     }
@@ -63,12 +85,13 @@ class MainActivity : TranslatorActivity(), InterstitialAdLoader by InterstitialA
     private fun initTextToSpeech() {
         initTextToSpeech(this) { status ->
             with(binding) {
+                val speakSourceText = sourceText.speakSourceText
+                val speakTranslatedText = translatedText.speakTranslatedText
+
                 if (status == TextToSpeech.SUCCESS) {
-                    speakSourceText.visible()
-                    speakTranslatedText.visible()
+                    visible(speakSourceText, speakTranslatedText)
                 } else {
-                    speakSourceText.gone()
-                    speakTranslatedText.gone()
+                    gone(speakSourceText, speakTranslatedText)
                 }
             }
         }
@@ -78,12 +101,13 @@ class MainActivity : TranslatorActivity(), InterstitialAdLoader by InterstitialA
         recognizeSpeech.rmsdB.update { rmsdB }
     }
 
-    private fun ActivityMainBinding.bind() {
-        materialToolbar()
-        drawerLayout()
-        materialButton()
-        navigationView()
-        nestedScrollView()
+    private fun ActivityMainBinding.bind(mainActivity: MainActivity) {
+        materialToolbar(mainActivity)
+
+        drawerLayout(mainActivity)
+        materialButton(mainActivity)
+        navigationView(activityResultLauncher, mainActivity)
+        nestedScrollView(mainActivity)
         speechRecognitionButton()
 
         bannerAd(adView, AdSize.BANNER)
@@ -98,9 +122,8 @@ class MainActivity : TranslatorActivity(), InterstitialAdLoader by InterstitialA
                     when {
                         checkPermission(Manifest.permission.RECORD_AUDIO) ->
                             startSpeechRecognition(sourceLanguage)
-                        shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO) -> {
-                            // todo 어쩔래/
-                        }
+                        shouldShowRequestPermissionRationale(Manifest.permission.RECORD_AUDIO) ->
+                            showRequestRecordAudioPermissionRationale()
                         else -> requestRecordAudioPermissionsLauncher.launch(Manifest.permission.RECORD_AUDIO)
                     }
                 }
@@ -109,47 +132,36 @@ class MainActivity : TranslatorActivity(), InterstitialAdLoader by InterstitialA
     }
 
     private fun MainViewModel.collect() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                adsRemoved.zip(characters) { adsRemoved, characters ->
-                    when {
-                        adsRemoved -> false
-                        characters < LIMIT_CHARACTERS -> false
-                        else -> true
-                    }
-                }.collect { condition ->
-                    if (condition) {
-                        showInterstitialAd(
-                            activity,
-                            onAdDismissedFullScreenContent = {
+        launchWithLifecycle {
+            adsRemoved.zip(characters) { adsRemoved, characters ->
+                when {
+                    adsRemoved -> false
+                    characters < integer(R.integer.limit_characters) -> false
+                    else -> true
+                }
+            }.collect { condition ->
+                if (condition) {
+                    showInterstitialAd(
+                        activity = activity,
+                        onAdDismissedFullScreenContent = {
+                            loadInterstitialAd(context)
+                        },
+                        onAdFailedToShowFullScreenContent = {
+                            if (it.code == FullScreenContentCallback.ERROR_CODE_AD_REUSED) {
                                 loadInterstitialAd(context)
-                            },
-                            onAdFailedToShowFullScreenContent = {
-                                if (it.code == FullScreenContentCallback.ERROR_CODE_AD_REUSED) {
-                                    loadInterstitialAd(context)
-                                }
                             }
-                        )
+                        }
+                    )
 
-                        clearCharacters()
-                    }
+                    clearCharacters()
                 }
             }
         }
 
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                val circularProgressIndicator = binding.circularProgressIndicator
-
-                translations.collect {
-                    when(it) {
-                        Result.Loading -> circularProgressIndicator.show()
-                        is Result.Success -> circularProgressIndicator.hide()
-                        is Result.Failure -> {
-                            circularProgressIndicator.hide()
-                            Toast.makeText(this@MainActivity, it.throwable.message, Toast.LENGTH_LONG).show()
-                        }
-                    }
+        launchWithLifecycle {
+            result.collect {
+                if (it is Result.Failure) {
+                    showToast(it.throwable)
                 }
             }
         }
