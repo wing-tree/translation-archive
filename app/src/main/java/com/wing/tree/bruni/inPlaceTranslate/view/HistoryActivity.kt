e682ac6131b28f2c379c9ff5ce669e8bd9d8638f
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-21 10:45:41 +0900

    feat : Implement history features

--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/HistoryActivity.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/view/HistoryActivity.kt
@@ -1,22 +1,39 @@
 package com.wing.tree.bruni.inPlaceTranslate.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.wing.tree.bruni.core.extension.launchWithLifecycle
+import com.wing.tree.bruni.core.regular.then
+import com.wing.tree.bruni.inPlaceTranslate.R
 import com.wing.tree.bruni.inPlaceTranslate.adapter.HistoryPagingDataAdapter
+import com.wing.tree.bruni.inPlaceTranslate.constant.EXTRA_HISTORY
 import com.wing.tree.bruni.inPlaceTranslate.databinding.ActivityHistoryBinding
 import com.wing.tree.bruni.inPlaceTranslate.viewModel.HistoryViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class HistoryActivity : AppCompatActivity() {
     private val binding by lazy { ActivityHistoryBinding.inflate(layoutInflater) }
-    private val historyPagingDataAdapter = HistoryPagingDataAdapter()
+    private val historyPagingDataAdapter = HistoryPagingDataAdapter(
+        object : HistoryPagingDataAdapter.OnClickListener {
+            override fun onItemClick(item: HistoryPagingDataAdapter.Item.History) {
+                val data = Intent().putExtra(EXTRA_HISTORY, item)
+
+                setResult(RESULT_OK, data).then {
+                    finish()
+                }
+            }
+
+            override fun onStarClick(rowid: Int, isFavorite: Boolean) {
+                viewModel.updateFavorite(rowid, isFavorite.not())
+            }
+        }
+    )
+
     private val viewModel by viewModels<HistoryViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,7 +44,24 @@ class HistoryActivity : AppCompatActivity() {
         viewModel.collect()
     }
 
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
+
     private fun ActivityHistoryBinding.bind() {
+        setSupportActionBar(materialToolbar)
+
+        supportActionBar?.let {
+            it.setTitle(R.string.history)
+            it.setDisplayShowHomeEnabled(true)
+            it.setDisplayHomeAsUpEnabled(true)
+        }
+
+        materialToolbar.setNavigationOnClickListener {
+            finish()
+        }
+
         recyclerView.apply {
             adapter = historyPagingDataAdapter
             layoutManager = LinearLayoutManager(context)
@@ -35,11 +69,9 @@ class HistoryActivity : AppCompatActivity() {
     }
 
     private fun HistoryViewModel.collect() {
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                historyPagingData.collect {
-                    historyPagingDataAdapter.submitData(it)
-                }
+        launchWithLifecycle {
+            historyPagingData.collect {
+                historyPagingDataAdapter.submitData(it)
             }
         }
     }
--- app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/viewModel/HistoryViewModel.kt
+++ app/src/main/java/com/wing/tree/bruni/inPlaceTranslate/viewModel/HistoryViewModel.kt
@@ -2,26 +2,66 @@ package com.wing.tree.bruni.inPlaceTranslate.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.cachedIn
-import androidx.paging.map
+import androidx.paging.*
+import com.wing.tree.bruni.core.extension.isNull
+import com.wing.tree.bruni.inPlaceTranslate.adapter.HistoryPagingDataAdapter
+import com.wing.tree.bruni.inPlaceTranslate.domain.useCase.UpdateFavoriteUseCase
 import com.wing.tree.bruni.inPlaceTranslate.mapper.HistoryMapper
-import com.wing.tree.bruni.inPlaceTranslate.data.entity.History as Entity
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import com.wing.tree.bruni.inPlaceTranslate.data.entity.History as Entity
 
 @HiltViewModel
 class HistoryViewModel @Inject constructor(
-    historyPager: Pager<Int, Entity>
+    historyPager: Pager<Int, Entity>,
+    private val updateFavoriteUseCase: UpdateFavoriteUseCase
 ) : ViewModel() {
+    private val generator = Generator()
+    private val ioDispatcher = Dispatchers.IO
     private val historyMapper = HistoryMapper()
 
-    val historyPagingData = historyPager.flow
+    val historyPagingData: Flow<PagingData<HistoryPagingDataAdapter.Item>> = historyPager.flow
         .map { pagingData ->
             pagingData.map { entity ->
                 historyMapper.toModel(entity)
+            }.insertSeparators { before, after ->
+                generator(before, after)
             }
         }
         .cachedIn(viewModelScope)
+
+    fun updateFavorite(rowid: Int, isFavorite: Boolean) {
+        viewModelScope.launch(ioDispatcher) {
+            updateFavoriteUseCase(
+                UpdateFavoriteUseCase.Parameter(
+                    rowid,
+                    isFavorite
+                )
+            )
+        }
+    }
+
+    private class Generator : (
+        HistoryPagingDataAdapter.Item.History?,
+        HistoryPagingDataAdapter.Item.History?
+    ) -> HistoryPagingDataAdapter.Item.TranslatedOn? {
+        override fun invoke(
+            before: HistoryPagingDataAdapter.Item.History?,
+            after: HistoryPagingDataAdapter.Item.History?
+        ): HistoryPagingDataAdapter.Item.TranslatedOn? {
+            return when {
+                before.isNull() -> after?.let {
+                    HistoryPagingDataAdapter.Item.TranslatedOn(it.translatedOn)
+                }
+                after.isNull() -> null
+                before.translatedOn.isAfter(after.translatedOn) ->
+                    HistoryPagingDataAdapter.Item.TranslatedOn(after.translatedOn)
+                else -> null
+            }
+        }
+    }
 }
