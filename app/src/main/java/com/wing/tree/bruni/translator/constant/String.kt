9e5dc33e3a26d8fbce31182f94b5598f816a7628
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-21 11:20:59 +0900

    refactor : Change string constant value

--- app/src/main/java/com/wing/tree/bruni/translator/constant/String.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/constant/String.kt
@@ -1,5 +1,5 @@
 package com.wing.tree.bruni.translator.constant
 
-private const val PACKAGE_NAME = "com.wing.tree.bruni.inPlaceTranslate.constant"
+private const val PACKAGE_NAME = "com.wing.tree.bruni.translator.constant"
 
 internal const val EXTRA_HISTORY = "$PACKAGE_NAME.HISTORY"
