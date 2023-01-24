70879af99241d5c7e95aa39f21df002440c1568b
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-24 22:04:45 +0900

    feat : Define new extra strings

--- app/src/main/java/com/wing/tree/bruni/translator/constant/String.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/constant/String.kt
@@ -2,5 +2,7 @@ package com.wing.tree.bruni.translator.constant
 
 private const val PACKAGE_NAME = "com.wing.tree.bruni.translator.constant"
 
+internal const val EXTRA_ENTER_ANIM = "$PACKAGE_NAME.ENTER_ANIM"
+internal const val EXTRA_EXIT_ANIM = "$PACKAGE_NAME.EXIT_ANIM"
 internal const val EXTRA_HISTORY = "$PACKAGE_NAME.HISTORY"
 internal const val EXTRA_LOAD_FAVORITES = "$PACKAGE_NAME.LOAD_FAVORITES"
