1fcf15059d74b0fa7bada1a4260559f4bddd4f87
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-24 19:40:05 +0900

    feat : Implement history delete feature

--- data/src/main/java/com/wing/tree/bruni/translator/data/repository/HistoryRepositoryImpl.kt
+++ data/src/main/java/com/wing/tree/bruni/translator/data/repository/HistoryRepositoryImpl.kt
@@ -1,17 +1,27 @@
 package com.wing.tree.bruni.translator.data.repository
 
+import com.wing.tree.bruni.translator.data.mapper.HistoryMapper
 import com.wing.tree.bruni.translator.data.source.local.HistoryDataSource
+import com.wing.tree.bruni.translator.domain.model.History
 import com.wing.tree.bruni.translator.domain.repository.HistoryRepository
 import javax.inject.Inject
 
 class HistoryRepositoryImpl @Inject constructor(
     private val historyDataSource: HistoryDataSource
 ) : HistoryRepository {
+    private val historyMapper = HistoryMapper()
+
     override suspend fun clearAll() {
         historyDataSource.clearAll()
+    }
+
+    override suspend fun delete(history: History) {
+        historyDataSource.delete(history.toEntity())
     }
 
     override suspend fun updateFavorite(rowid: Int, isFavorite: Boolean) {
         historyDataSource.updateFavorite(rowid, isFavorite)
     }
+
+    private fun History.toEntity() = historyMapper.toEntity(this)
 }
--- domain/src/main/java/com/wing/tree/bruni/translator/domain/repository/HistoryRepository.kt
+++ domain/src/main/java/com/wing/tree/bruni/translator/domain/repository/HistoryRepository.kt
@@ -1,6 +1,9 @@
 package com.wing.tree.bruni.translator.domain.repository
 
+import com.wing.tree.bruni.translator.domain.model.History
+
 interface HistoryRepository {
     suspend fun clearAll()
+    suspend fun delete(history: History)
     suspend fun updateFavorite(rowid: Int, isFavorite: Boolean)
 }
