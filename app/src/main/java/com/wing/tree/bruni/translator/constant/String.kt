1c5d1bc3e3f3727acd4f14638f40e71c058514fa
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-24 19:16:42 +0900

    feat : Add string values

--- app/src/main/java/com/wing/tree/bruni/translator/constant/String.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/constant/String.kt
@@ -3,3 +3,4 @@ package com.wing.tree.bruni.translator.constant
 private const val PACKAGE_NAME = "com.wing.tree.bruni.translator.constant"
 
 internal const val EXTRA_HISTORY = "$PACKAGE_NAME.HISTORY"
+internal const val EXTRA_LOAD_FAVORITES = "$PACKAGE_NAME.LOAD_FAVORITES"
