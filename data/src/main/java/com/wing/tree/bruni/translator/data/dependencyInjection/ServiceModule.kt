a2b066b79e9f9398977b62d64cca01b57bbaa4df
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-26 07:18:01 +0900

    feat : Declare billing service to hilt module

--- data/src/main/java/com/wing/tree/bruni/translator/data/dependencyInjection/ServiceModule.kt
+++ data/src/main/java/com/wing/tree/bruni/translator/data/dependencyInjection/ServiceModule.kt
@@ -1,9 +1,13 @@
 package com.wing.tree.bruni.translator.data.dependencyInjection
 
+import android.content.Context
+import com.wing.tree.bruni.billing.BillingService
+import com.wing.tree.bruni.translator.data.billing.products
 import com.wing.tree.bruni.translator.data.service.TranslationService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -11,6 +15,12 @@ import javax.inject.Singleton
 @InstallIn(SingletonComponent::class)
 @Module
 internal object ServiceModule {
+    @Provides
+    @Singleton
+    fun providesBillingService(@ApplicationContext context: Context): BillingService {
+        return BillingService(context, products)
+    }
+
     @Provides
     @Singleton
     fun providesTranslationService(retrofit: Retrofit): TranslationService {
