26ca0dbc108fc1d09685ba903dd1c1f35b98fcca
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-01-29 19:33:57 +0900

    feat : Implement Modifier extension functions

--- app/src/main/java/com/wing/tree/bruni/translator/extension/Modifier.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/extension/Modifier.kt
@@ -6,4 +6,9 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.wing.tree.bruni.core.constant.ZERO
 
+fun Modifier.paddingStart(start: Dp) = padding(start = start)
+fun Modifier.paddingTop(top: Dp) = padding(top = top)
+fun Modifier.paddingEnd(end: Dp) = padding(end = end)
+fun Modifier.paddingBottom(bottom: Dp) = padding(bottom = bottom)
 fun Modifier.paddingHorizontal(horizontal: Dp) = padding(horizontal, ZERO.dp)
+fun Modifier.paddingVertical(vertical: Dp) = padding(ZERO.dp, vertical)
