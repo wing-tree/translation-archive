384d81c7b6ae6f9d23ac650e90a20551ea3b274d
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-10 00:11:05 +0900

    refactor : Modify architecture

---
+++ data/src/main/java/com/wing/tree/bruni/translator/data/billing/ProductID.kt
@@ -0,0 +1,5 @@
+package com.wing.tree.bruni.translator.data.billing
+
+internal object ProductID {
+    const val remove_ads = "remove_ads"
+}
--- data/src/main/java/com/wing/tree/bruni/translator/data/billing/Products.kt
+++ data/src/main/java/com/wing/tree/bruni/translator/data/billing/Products.kt
@@ -2,15 +2,10 @@ package com.wing.tree.bruni.translator.data.billing
 
 import com.wing.tree.bruni.billing.model.Product
 import com.wing.tree.bruni.billing.model.Type
-import com.wing.tree.bruni.translator.data.billing.Product.RemoveAds
-
-object Product {
-    val RemoveAds = Product(
-        id = "remove_ads",
-        type = Type.INAPP(consumable = false)
-    )
-}
 
 val products = listOf(
-    RemoveAds
+    Product(
+        id = ProductID.remove_ads,
+        type = Type.INAPP(consumable = false)
+    )
 )
