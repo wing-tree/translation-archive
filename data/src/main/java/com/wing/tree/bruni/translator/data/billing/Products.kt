15bfd02a483be443c062dbcee9a1129a36fb45b0
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-13 01:32:06 +0900

    update : Update library versions

--- data/src/main/java/com/wing/tree/bruni/translator/data/billing/Products.kt
+++ data/src/main/java/com/wing/tree/bruni/translator/data/billing/Products.kt
@@ -1,11 +1,10 @@
 package com.wing.tree.bruni.translator.data.billing
 
 import com.wing.tree.bruni.billing.model.Product
-import com.wing.tree.bruni.billing.model.Type
 
 val products = listOf(
-    Product(
-        id = ProductID.remove_ads,
-        type = Type.INAPP(consumable = false)
+    Product.INAPP(
+        id = ProductId.remove_ads,
+        consumable = false
     )
 )
