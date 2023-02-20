ffdf31912b78041e9837a6678dfad49269c16cc0
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-20 22:35:56 +0900

    feat : Add purchase handling logic

--- app/src/main/java/com/wing/tree/bruni/translator/viewModel/MainViewModel.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/viewModel/MainViewModel.kt
@@ -1,7 +1,11 @@
 package com.wing.tree.bruni.translator.viewModel
 
 import androidx.lifecycle.viewModelScope
+import com.android.billingclient.api.BillingClient.ProductType
+import com.wing.tree.bruni.billing.BillingService
+import com.wing.tree.bruni.billing.BillingService.Companion.purchased
 import com.wing.tree.bruni.core.useCase.getOrDefault
+import com.wing.tree.bruni.translator.data.billing.ProductId
 import com.wing.tree.bruni.translator.domain.model.History
 import com.wing.tree.bruni.translator.domain.useCase.*
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,9 +16,10 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    adsRemovedUseCase: AdsRemovedUseCase,
     charactersUseCase: CharactersUseCase,
     translateUseCase: TranslateUseCase,
+    private val adsRemovedUseCase: AdsRemovedUseCase,
+    private val billingService: BillingService,
     private val clearAllHistoriesUseCase: ClearAllHistoriesUseCase,
     private val sourceUseCase: SourceUseCase,
     private val targetUseCase: TargetUseCase
@@ -26,10 +31,32 @@ class MainViewModel @Inject constructor(
 ) {
     val adsRemoved = adsRemovedUseCase.get().map { it.getOrDefault(false) }
 
+    init {
+        viewModelScope.launch {
+            with(billingService) {
+                for (purchase in purchases) {
+                    val value = purchase.orNull() ?: continue
+
+                    if (value.purchased) {
+                        processPurchase(value).orNull()?.let {
+                            if (ProductId.remove_ads in it.products) {
+                                adsRemovedUseCase.put(true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     fun clearAllHistories() {
         viewModelScope.launch(ioDispatcher) {
             clearAllHistoriesUseCase()
         }
+    }
+
+    fun queryPurchases(@ProductType productType: String) {
+        billingService.queryPurchases(productType)
     }
 
     fun translateHistory(history: History) {
