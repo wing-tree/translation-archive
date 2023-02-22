340f6ec9a987b067202c5a4fe9722abe23b5ea95
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-23 08:31:42 +0900

    refactor : Improve text resizing logic

--- app/src/main/java/com/wing/tree/bruni/translator/constant/Int.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/constant/Int.kt
@@ -1,4 +1,10 @@
 package com.wing.tree.bruni.translator.constant
 
-internal const val LARGE_TEXT_MAX_LINES = 3
-internal const val MEDIUM_TEXT_MAX_LINES = 4
+import com.wing.tree.bruni.core.constant.*
+
+internal const val EXTRA_EXTRA_LARGE_TEXT_MAX_LINES = TWO
+internal const val EXTRA_LARGE_TEXT_MAX_LINES = THREE
+internal const val LARGE_TEXT_MAX_LINES = FOUR
+internal const val MEDIUM_TEXT_MAX_LINES = FIVE
+internal const val SMALL_TEXT_MAX_LINES = SIX
+internal const val EXTRA_SMALL_TEXT_MAX_LINES = SEVEN
