d0372e8f8a20ab4610def589f35d79a1197e9ea4
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-24 23:13:20 +0900

    feat : Hide keyboard when share button clicked

--- app/src/main/java/com/wing/tree/bruni/translator/view/TranslatorActivity.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/view/TranslatorActivity.kt
@@ -7,11 +7,13 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.speech.SpeechRecognizer
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.annotation.CallSuper
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.wing.tree.bruni.core.constant.EMPTY
 import com.wing.tree.bruni.core.constant.SCHEME_PACKAGE
+import com.wing.tree.bruni.core.constant.ZERO
 import com.wing.tree.bruni.core.extension.*
 import com.wing.tree.bruni.translator.R
 import com.wing.tree.bruni.translator.delegate.TextToSpeechDelegate
@@ -23,6 +25,10 @@ import java.util.*
 
 abstract class TranslatorActivity : SpeechRecognizerActivity(), TextToSpeechDelegate by TextToSpeechDelegateImpl() {
     abstract val viewModel: TranslatorViewModel
+
+    private val inputMethodManager by lazy {
+        getSystemService(InputMethodManager::class.java)
+    }
 
     private val sourceLocale: Locale get() = viewModel.sourceLocale
     private val targetLocale: Locale get() = viewModel.targetLocale
@@ -117,6 +123,10 @@ abstract class TranslatorActivity : SpeechRecognizerActivity(), TextToSpeechDele
 
     fun shareTranslatedText() {
         translatedText?.ifNotBlank { text ->
+            currentFocus?.let {
+                inputMethodManager?.hideSoftInputFromWindow(it.windowToken, ZERO)
+            }
+
             sharePlainText(text)
         }
     }
