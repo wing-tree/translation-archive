468f6db67bad48c4cc95981bd2476e2db3651d31
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-27 07:46:30 +0900

    refactor : Remove unused code

--- app/src/main/java/com/wing/tree/bruni/translator/extension/SpeechRecognizer.kt
+++
@@ -1,9 +0,0 @@
-package com.wing.tree.bruni.translator.extension
-
-import android.speech.SpeechRecognizer
-
-internal fun SpeechRecognizer.clear() {
-    stopListening()
-    cancel()
-    destroy()
-}
