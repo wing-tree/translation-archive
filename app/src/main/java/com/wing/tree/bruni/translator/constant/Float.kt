14f8d57798aad9769e531043ec68783cc8a7a515
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-27 07:49:26 +0900

    refactor : Cleanup

--- app/src/main/java/com/wing/tree/bruni/translator/constant/Float.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/constant/Float.kt
@@ -1,5 +1,4 @@
 package com.wing.tree.bruni.translator.constant
 
-internal const val DEGREE_180 = 180.0F
 internal const val PITCH = 1.0F
 internal const val SPEECH_RATE = 1.0F
--- app/src/main/java/com/wing/tree/bruni/translator/constant/Int.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/constant/Int.kt
@@ -1,6 +1,7 @@
 package com.wing.tree.bruni.translator.constant
 
-import com.wing.tree.bruni.core.constant.*
+import com.wing.tree.bruni.core.constant.FOUR
+import com.wing.tree.bruni.core.constant.TWO
 
 internal const val LARGE_TEXT_MAX_LINES = TWO
 internal const val MEDIUM_TEXT_MAX_LINES = FOUR
