252276d69b04a52311b09ad728d0212943d08471
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-26 15:00:47 +0900

    refactor : Modify text resizing logic

--- app/src/main/java/com/wing/tree/bruni/translator/constant/Int.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/constant/Int.kt
@@ -2,9 +2,7 @@ package com.wing.tree.bruni.translator.constant
 
 import com.wing.tree.bruni.core.constant.*
 
-internal const val EXTRA_EXTRA_LARGE_TEXT_MAX_LINES = TWO
-internal const val EXTRA_LARGE_TEXT_MAX_LINES = THREE
-internal const val LARGE_TEXT_MAX_LINES = FOUR
-internal const val MEDIUM_TEXT_MAX_LINES = FIVE
-internal const val SMALL_TEXT_MAX_LINES = SIX
-internal const val EXTRA_SMALL_TEXT_MAX_LINES = SEVEN
+internal const val EXTRA_LARGE_TEXT_MAX_LINES = TWO
+internal const val LARGE_TEXT_MAX_LINES = THREE
+internal const val MEDIUM_TEXT_MAX_LINES = FOUR
+internal const val SMALL_TEXT_MAX_LINES = FIVE
--- app/src/main/java/com/wing/tree/bruni/translator/extension/TextView.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/extension/TextView.kt
@@ -6,25 +6,21 @@ import com.wing.tree.bruni.core.extension.lineCount
 import com.wing.tree.bruni.translator.R
 import com.wing.tree.bruni.translator.constant.*
 
-private val TextView.extraExtraLargeTextSize: Float get() = resources.dimen(R.dimen.extra_extra_large_text_size)
 private val TextView.extraLargeTextSize: Float get() = resources.dimen(R.dimen.extra_large_text_size)
 private val TextView.largeTextSize: Float get() = resources.dimen(R.dimen.large_text_size)
 private val TextView.mediumTextSize: Float get() = resources.dimen(R.dimen.medium_text_size)
 private val TextView.smallTextSize: Float get() = resources.dimen(R.dimen.small_text_size)
 private val TextView.extraSmallTextSize: Float get() = resources.dimen(R.dimen.extra_small_text_size)
-private val TextView.extraExtraSmallTextSize: Float get() = resources.dimen(R.dimen.extra_extra_small_text_size)
 
 internal fun TextView.resizeText(): Float {
     val scaledDensity = resources.displayMetrics.scaledDensity
 
     return when {
-        lineCount(extraExtraLargeTextSize) <= EXTRA_EXTRA_LARGE_TEXT_MAX_LINES -> extraExtraLargeTextSize
         lineCount(extraLargeTextSize) <= EXTRA_LARGE_TEXT_MAX_LINES -> extraLargeTextSize
         lineCount(largeTextSize) <= LARGE_TEXT_MAX_LINES -> largeTextSize
         lineCount(mediumTextSize) <= MEDIUM_TEXT_MAX_LINES -> mediumTextSize
         lineCount(smallTextSize) <= SMALL_TEXT_MAX_LINES -> smallTextSize
-        lineCount(extraSmallTextSize) <= EXTRA_SMALL_TEXT_MAX_LINES -> extraSmallTextSize
-        else -> extraExtraSmallTextSize
+        else -> extraSmallTextSize
     }
         .div(scaledDensity)
         .also {
