b47d9235e15730ab6dd6a3c69102df3851a51173
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-26 22:21:47 +0900

    refactor : Modify text resize logic

--- app/src/main/java/com/wing/tree/bruni/translator/constant/Int.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/constant/Int.kt
@@ -2,7 +2,5 @@ package com.wing.tree.bruni.translator.constant
 
 import com.wing.tree.bruni.core.constant.*
 
-internal const val EXTRA_LARGE_TEXT_MAX_LINES = TWO
-internal const val LARGE_TEXT_MAX_LINES = THREE
+internal const val LARGE_TEXT_MAX_LINES = TWO
 internal const val MEDIUM_TEXT_MAX_LINES = FOUR
-internal const val SMALL_TEXT_MAX_LINES = FIVE
--- app/src/main/java/com/wing/tree/bruni/translator/extension/TextView.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/extension/TextView.kt
@@ -6,21 +6,17 @@ import com.wing.tree.bruni.core.extension.lineCount
 import com.wing.tree.bruni.translator.R
 import com.wing.tree.bruni.translator.constant.*
 
-private val TextView.extraLargeTextSize: Float get() = resources.dimen(R.dimen.extra_large_text_size)
 private val TextView.largeTextSize: Float get() = resources.dimen(R.dimen.large_text_size)
 private val TextView.mediumTextSize: Float get() = resources.dimen(R.dimen.medium_text_size)
 private val TextView.smallTextSize: Float get() = resources.dimen(R.dimen.small_text_size)
-private val TextView.extraSmallTextSize: Float get() = resources.dimen(R.dimen.extra_small_text_size)
 
 internal fun TextView.resizeText(): Float {
     val scaledDensity = resources.displayMetrics.scaledDensity
 
     return when {
-        lineCount(extraLargeTextSize) <= EXTRA_LARGE_TEXT_MAX_LINES -> extraLargeTextSize
         lineCount(largeTextSize) <= LARGE_TEXT_MAX_LINES -> largeTextSize
         lineCount(mediumTextSize) <= MEDIUM_TEXT_MAX_LINES -> mediumTextSize
-        lineCount(smallTextSize) <= SMALL_TEXT_MAX_LINES -> smallTextSize
-        else -> extraSmallTextSize
+        else -> smallTextSize
     }
         .div(scaledDensity)
         .also {
