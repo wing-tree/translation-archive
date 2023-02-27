719cf9b3029d77081662b27a6852c27ae5cd09cd
SEONGJU KIM <wind.orca.pe@gmail.com>
2023-02-28 07:56:24 +0900

    fix : Fix build error

--- app/src/main/java/com/wing/tree/bruni/translator/view/dataBinding/ActivityProcessTextBinding.kt
+++ app/src/main/java/com/wing/tree/bruni/translator/view/dataBinding/ActivityProcessTextBinding.kt
@@ -3,20 +3,28 @@ package com.wing.tree.bruni.translator.view.dataBinding
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import androidx.core.widget.addTextChangedListener
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsAnimationCompat
+import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.wing.tree.bruni.core.constant.ONE
 import com.wing.tree.bruni.core.constant.ZERO
 import com.wing.tree.bruni.core.extension.*
 import com.wing.tree.bruni.translator.R
 import com.wing.tree.bruni.translator.databinding.ActivityProcessTextBinding
-import com.wing.tree.bruni.translator.extension.resizeText
-import com.wing.tree.bruni.translator.view.*
+import com.wing.tree.bruni.translator.view.ProcessTextActivity
+import com.wing.tree.bruni.windowInsetsAnimation.extension.isTypeMasked
 
 internal fun ActivityProcessTextBinding.bottomSheet(processTextActivity: ProcessTextActivity) {
+    bottomSheet.updateHeight {
+        (screenSize.height * 0.75f).int
+    }
+
     val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
     val delayMillis = processTextActivity.configShortAnimTime.long
 
+    bottomSheetBehavior.peekHeight = screenSize.height//bottomSheet.height
     bottomSheetBehavior.skipCollapsed = true
     bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
 
@@ -24,8 +32,11 @@ internal fun ActivityProcessTextBinding.bottomSheet(processTextActivity: Process
         {
             bottomSheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
-                    if (newState == BottomSheetBehavior.STATE_HIDDEN) {
-                        processTextActivity.finish()
+                    when (newState) {
+                        BottomSheetBehavior.STATE_HIDDEN -> {
+                            processTextActivity.finish()
+                        }
+                        else -> Unit
                     }
                 }
 
@@ -38,49 +49,40 @@ internal fun ActivityProcessTextBinding.bottomSheet(processTextActivity: Process
     )
 }
 
-internal fun ActivityProcessTextBinding.editText() = with(sourceText) {
-    with(sourceText) {
-        addTextChangedListener {
-            val textSize = resizeText()
 
-            with(translatedText) {
-                translatedText.textSize = textSize
-            }
-        }
-    }
-}
-
-internal fun ActivityProcessTextBinding.materialButton(
+internal fun ActivityProcessTextBinding.button(
     processTextActivity: ProcessTextActivity
-) = with(processTextActivity) {
-    val swapLanguages = sourceText.swapLanguages
-    val displaySourceLanguage = sourceText.displaySourceLanguage
-    val displayTargetLanguage = translatedText.displayTargetLanguage
+) {
+    with(processTextActivity) {
+        val swapLanguages = sourceText.swapLanguages
+        val displaySourceLanguage = sourceText.displaySourceLanguage
+        val displayTargetLanguage = translatedText.displayTargetLanguage
 
-    swapLanguages.setOnClickListener {
-        swapLanguages.isClickable = false
-        swapLanguages.isFocusable = false
+        swapLanguages.setOnClickListener {
+            swapLanguages.isClickable = false
+            swapLanguages.isFocusable = false
 
-        val accelerateQuadInterpolator = accelerateQuadInterpolator
-        val decelerateQuadInterpolator = decelerateQuadInterpolator
-        val duration = configShortAnimTime.long.half
-        val translationY = dimen(R.dimen.text_size_22dp)
+            val accelerateQuadInterpolator = accelerateQuadInterpolator
+            val decelerateQuadInterpolator = decelerateQuadInterpolator
+            val duration = configShortAnimTime.long.half
+            val translationY = dimen(R.dimen.text_size_20dp)
 
-        with(displaySourceLanguage) {
-            translateDown(duration, translationY, accelerateQuadInterpolator) {
-                processTextActivity.swapLanguages()
-                translateUp(duration, ZERO.float, decelerateQuadInterpolator) {
-                    swapLanguages.isClickable = true
-                    swapLanguages.isFocusable = true
-                }.alpha(ONE.float)
-            }.alpha(ZERO.float)
-        }
+            with(displaySourceLanguage) {
+                translateY(translationY, duration, accelerateQuadInterpolator) {
+                    swapLanguages()
+                    translateY(ZERO.float, duration, decelerateQuadInterpolator) {
+                        swapLanguages.isClickable = true
+                        swapLanguages.isFocusable = true
+                    }.alpha(ONE.float)
+                }.alpha(ZERO.float)
+            }
 
-        with(displayTargetLanguage) {
-            translateUp(duration, translationY, accelerateQuadInterpolator) {
-                translateDown(duration, ZERO.float, decelerateQuadInterpolator)
-                    .alpha(ONE.float)
-            }.alpha(ZERO.float)
+            with(displayTargetLanguage) {
+                translateY(translationY.negative, duration, accelerateQuadInterpolator) {
+                    translateY(ZERO.float, duration, decelerateQuadInterpolator)
+                        .alpha(ONE.float)
+                }.alpha(ZERO.float)
+            }
         }
     }
 }
@@ -88,4 +90,95 @@ internal fun ActivityProcessTextBinding.materialButton(
 internal fun ActivityProcessTextBinding.nestedScrollView(processTextActivity: ProcessTextActivity) {
     sourceText.nestedScrollView(processTextActivity)
     translatedText.nestedScrollView(processTextActivity)
+}
+
+internal fun ActivityProcessTextBinding.sourceText() {
+    with(sourceText) {
+        doAfterTextChanged {
+            val textSize = resizeText()
+
+            with(translatedText) {
+                translatedText.textSize = textSize
+            }
+        }
+
+        setWindowInsetsAnimationCallback()
+    }
+}
+
+internal fun ActivityProcessTextBinding.setWindowInsetsAnimationCallback() {
+    post {
+        ViewCompat.setWindowInsetsAnimationCallback(
+            root,
+            object : WindowInsetsAnimationCompat.Callback(DISPATCH_MODE_CONTINUE_ON_SUBTREE) {
+                private var adRatio: Float = ZERO.float
+                private val h1 = bottomSheet.height
+                private var ratio1 = ZERO.float
+
+                override fun onStart(
+                    animation: WindowInsetsAnimationCompat,
+                    bounds: WindowInsetsAnimationCompat.BoundsCompat
+                ): WindowInsetsAnimationCompat.BoundsCompat {
+                    if (animation.isTypeMasked(WindowInsetsCompat.Type.ime())) {
+                        val windowInsets = ViewCompat.getRootWindowInsets(root) ?: return bounds
+
+                        if (windowInsets.isVisible(WindowInsetsCompat.Type.ime())) {
+                            Insets.subtract(
+                                windowInsets.getInsets(WindowInsetsCompat.Type.ime()),
+                                windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+                            ).let {
+                                Insets.max(it, Insets.NONE)
+                            }.let {
+
+                                //adRatio = 1f.safeDiv(it.bottom)
+
+                                ratio1 = h1
+                                    .minus(screenSize.height)
+                                    .float
+                                    .safeDiv(it.bottom)
+
+                                adRatio = 96f.float.safeDiv(it.bottom)
+                            }
+                        }
+                    }
+
+                    return bounds
+                }
+
+                override fun onProgress(
+                    insets: WindowInsetsCompat,
+                    runningAnimations: MutableList<WindowInsetsAnimationCompat>
+                ): WindowInsetsCompat {
+                    Insets.subtract(
+                        insets.getInsets(WindowInsetsCompat.Type.ime()),
+                        insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                    ).let {
+                        Insets.max(it, Insets.NONE)
+                    }.let {
+                        it.top.minus(it.bottom)
+                    }.let {
+
+                        adView.translationY = -it.float//1f.plus(ratio1).negative.times(it)
+
+                        bottomSheet.translationY = it.float//ratio1.negative.times(it)
+                        bottomSheet.updateHeight { _ ->
+                            h1.plus(1f.plus(ratio1).times(it)).int
+                        }
+
+                        adView.updateHeight { _ ->
+                            96f.plus(adRatio.times(it)).int
+                        }
+
+                        return insets
+                    }
+                }
+
+                override fun onEnd(animation: WindowInsetsAnimationCompat) {
+                    if (animation.isTypeMasked(WindowInsetsCompat.Type.ime())) {
+                        bottomSheet.translationY = ZERO.float
+                    }
+                }
+            }
+        )
+    }
 }
